af37519b9c44fae0bca4658139d16d3259f3a829
woogie333 <khw030303@gmail.com>
2026-03-09 22:02:36 +0900

    기본 메인 화면 구성

--- ChickenStock/src/App.tsx
+++ ChickenStock/src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import StockChart from './pages/StockChart';
+import React, { useState } from 'react';
+import Header from './pages/header';
+import MainBoard from './pages/mainboard';
 
-const App: React.FC = () => {
+export default function App() {
+  // 메인 화면에 띄울 주식 정보를 관리하는 상태입니다.
+  const [currentStock, setCurrentStock] = useState({
+    name: "삼성전자",
+    rate: "-5.29%"
+  });
+
   return (
-        <StockChart />
+    <div className="app-container">
+      <div className="white-background">
+        {/* 로그인 로직을 제외했으므로, 임시로 alert 창을 띄우는 함수를 전달합니다.
+          이렇게 하면 HeaderProps의 타입 규칙을 어기지 않아 에러가 나지 않습니다. 
+        */}
+        <Header onOpenLogin={() => alert('로그인 기능은 준비 중입니다.')} />
+        
+        {/* MainBoard 컴포넌트에 현재 주식 정보를 Props로 건네줍니다. */}
+        <MainBoard 
+          stockName={currentStock.name} 
+          changeRate={currentStock.rate} 
+        />
+      </div>
+    </div>
   );
-};
-
-
-export default App;
+}
